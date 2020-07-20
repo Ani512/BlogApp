479127825809e24b986edaf9ad03b4d0e5fb7f1c
Anirudh Umarji <anirudh.umarji@gmail.com>
2020-07-20 07:36:19 +0530

    03_HTML_Templates

--- Blog_Clone/Blog_Clone/settings.py
+++ Blog_Clone/Blog_Clone/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Blog_Clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["os.path.join(SETTINGS_PATH, 'templates')"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Blog_Clone/blog/views.py
+++ Blog_Clone/blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Anirudh Umarji',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Rahul Raj',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'November 5, 2018'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/index.html', context)
 
 
 def about(request):
-    return HttpResponse('<h2>Blog About</h2>')
+    return render(request, 'blog/about.html')
