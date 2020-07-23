8ccf80eb7d87644ad956062ef44c23f485bb50a1
Anirudh Umarji <anirudh.umarji@gmail.com>
2020-07-23 18:15:07 +0530

    07_Login_Logout

--- Blog_Clone/users/urls.py
+++ Blog_Clone/users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.register, name='register'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('', auth_views.LoginView.as_view(redirect_authenticated_user=True,
+                                          template_name='users/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
